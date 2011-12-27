a8bd1b5c891c5ebc281a1e162a6f1d847dc3714a
loga jegede <loga.jegede@gmail.com>
2011-12-27 22:52:35 +0000

    tests proved nothing. start again

--- features/step_definitions/verbs_steps.rb
+++
@@ -1,7 +0,0 @@
-When /^I add the verb "([^"]*)" with meaning "([^"]*)"$/ do |verb, meaning|
-  visit 'new/verb'
-
-  fill_in('verb_name', :with => verb)
-  fill_in('verb_meaning', :with => meaning)
-  click_button 'Add Verb'
-end
